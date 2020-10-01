361add2ddbfce26022046b20eb71886ee401b94f
Nemanja <mr.nemanja.marjanovic@gmail.com>
2020-10-01 10:20:19 +0000

    Course scaffold title description, action text

---
+++ app/models/course.rb
@@ -0,0 +1,13 @@
+class Course < ApplicationRecord
+  
+  validates :title, presence: true
+  validates :description, presence: true, length: { :minimum => 5 }
+
+  
+  def to_s
+    title
+  end
+  
+    has_rich_text :description
+    
+end
